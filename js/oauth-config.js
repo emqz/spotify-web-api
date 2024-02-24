d73d0bcbbce0977ea7cd0c11e539d562986e8987
emqz <131368100+emqz@users.noreply.github.com>
2024-02-24 15:05:04 +0000

    changed client id and redirect uri

--- js/oauth-config.js
+++ js/oauth-config.js
@@ -4,8 +4,8 @@ var OAuthConfig = (function() {
 
   /* replace these values with yours obtained in the
   "My Applications" section of the Spotify developer site */
-  var clientId = '59d50f7e590b45e09b28574d86b56d06';
-  var redirectUri = 'http://localhost:8000/callback.html';
+  var clientId = 'be4b6717e3a54772a4b60a37b8cc5329';
+  var redirectUri = 'http://localhost:5502/callback';
 
   if (location.href.indexOf('http://jmperezperez.com') === 0) {
     redirectUri = 'http://jmperezperez.com/spotify-web-api-start-template/callback.html';
