fc18d079311b4c93ae1de2054fb0ae3856ede0e2
emqz <131368100+emqz@users.noreply.github.com>
2024-02-25 18:57:14 +0000

    changed ip address

--- js/oauth-config.js
+++ js/oauth-config.js
@@ -4,8 +4,8 @@ var OAuthConfig = (function() {
 
   /* replace these values with yours obtained in the
   "My Applications" section of the Spotify developer site */
-  var clientId = 'be4b6717e3a54772a4b60a37b8cc5329';
-  var redirectUri = 'http://localhost:5502/callback';
+  var clientId = '6982890b5bc2461a88d0a1a92598bb6e';
+  var redirectUri = 'http://127.0.0.1:5500/callback';
 
   if (location.href.indexOf('http://jmperezperez.com') === 0) {
     redirectUri = 'http://jmperezperez.com/spotify-web-api-start-template/callback.html';
